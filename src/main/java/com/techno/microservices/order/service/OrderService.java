24897ca0abdeae41d09e3a2ccd349cda69681f67
Apisit <amornchanchaigul@gmaul.com>
2025-01-24 23:57:00 +0700

    Integrating Kafka with Schema Registry

--- src/main/java/com/techno/microservices/order/service/OrderService.java
+++ src/main/java/com/techno/microservices/order/service/OrderService.java
@@ -2,19 +2,24 @@ package com.techno.microservices.order.service;
 
 import com.techno.microservices.order.client.InventoryClient;
 import com.techno.microservices.order.dto.OrderRequest;
+import com.techno.microservices.order.event.OrderPlacedEvent;
 import com.techno.microservices.order.model.Order;
 import com.techno.microservices.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Log4j2
 public class OrderService {
 
     private final OrderRepository orderRepository;
     private final InventoryClient inventoryClient;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public void placeOrder(OrderRequest orderRequest) {
         var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
@@ -25,6 +30,15 @@ public class OrderService {
             order.setSkuCode(orderRequest.skuCode());
             order.setQuantity(orderRequest.quantity());
             orderRepository.save(order);
+            // Send the message to kafka Topic
+            OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent();
+            orderPlacedEvent.setOrderNumber(order.getOrderNumber());
+            orderPlacedEvent.setEmail(orderRequest.userDetails().email());
+            orderPlacedEvent.setFirstName(orderRequest.userDetails().firstName());
+            orderPlacedEvent.setLastName(orderRequest.userDetails().lastName());
+            log.info("Start sending OrderPlacedEvent {}", orderPlacedEvent);
+            kafkaTemplate.send("order-placed", orderPlacedEvent);
+            log.info("End sending OrderPlacedEvent {}", orderPlacedEvent);
         } else {
             throw new RuntimeException("Product with SkuCode " + orderRequest.skuCode() + " is not in stock");
         }
